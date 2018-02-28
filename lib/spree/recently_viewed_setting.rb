e123d711a88edb2ef65b2e0e7c4fc9d39667099e
Alex Costantini <therealcos@gmail.com>
2018-02-28 12:54:35 -0500

    wip

--- lib/spree/recently_viewed_setting.rb
+++ lib/spree/recently_viewed_setting.rb
@@ -1,5 +1,5 @@
 module Spree
   class RecentlyViewedSetting < Preferences::Configuration
-    preference :recently_viewed_products_max_count, :integer, default: 8
+    preference :recently_viewed_products_max_count, :integer, default: 10
   end
 end
